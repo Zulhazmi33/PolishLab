a7e151c7b3d5f5083a12c2963061cae679c65e54
Raja Ahmad Zulhazmi <lazicdeveloper.hq@gmail.com>
2026-06-10 14:34:43 +0800

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'PolishLab',
+        short_name: 'PolishLab',
+        description: 'Simple ai integration app',
+        theme_color: '#ffffff',
+
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
